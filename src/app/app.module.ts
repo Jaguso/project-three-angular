7be15b9353d7c358f1cfd07b0b025bdd55636100
Jaguso <jagutzo@gmail.com>
2019-02-04 00:04:39 -0600

    configured router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
+
+const routes: Routes = [
+  { path: 'create', component: CreateUserComponent },
+  { path: 'show', component: UsersListComponent },
+  { path: '', redirectTo: 'create', pathMatch: 'full' }
+];
+
 
 @NgModule({
   declarations: [
@@ -15,7 +23,8 @@ import { UsersListComponent } from './components/users-list/users-list.component
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
